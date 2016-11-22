4602d1b26afa68dec0d6c8e20f2ff49a8865283e
whfzd <xiayu0615@foxmail.com>
2016-11-22 10:36:09 +0800

    whfzd
    更新

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var path = require("path"); //引入 path 中间件
 
 var app = express(); //运行express
 
-//将url地址指向项目中静态文件路径
-app.use('/',express.static(__dirname + '/views'));
-app.use('/static',express.static(__dirname + '/static'));
+//开发环境
+app.use('/dev/views', express.static(__dirname + '/views'))
+app.use('/dev/static', express.static(__dirname + '/static'))
+
+//部署环境
+app.use('/', express.static(__dirname + '/dist/views'))
+app.use('/static', express.static(__dirname + '/dist/static'))
 
 //启动一个服务 传入express 设置端口
-http.createServer(app).listen('9888',function()
+http.createServer(app).listen('16081',function()
 {
-	console.log('server start at 9888');
+	console.log('server start at 16081');
 })
